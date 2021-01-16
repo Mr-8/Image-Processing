11e08635b9f3a5a7f6cb29a84786d148a30c1618
Rubing Han <46811755+Mr-8@users.noreply.github.com>
2021-01-16 23:12:28 +0800

    Add files via upload

--- Testdemo.m
+++ Testdemo.m
@@ -25,13 +25,14 @@ gaussian_sigma = 1;
 para.noisesigma = 0.05; %% noise level
 para.sigma = 0.05;
 para.k = 2;  %% TGV order
+para.toler = 1e-4;
 para.subtoler = 1e-4;
 para.eta = 0.05;
 para.maxIter = 500;
 para.alpha0 = 0.1;
 para.alpha1 = 0.05;
 para.tau = 0.02;
-
+para.InnerIter = 500;
 %% Initial picture and noise
 f = im2double(imread('peppers256.png'));
 [para.size1,para.size2] = size(f);
@@ -40,18 +41,46 @@ noised = f + para.noisesigma*randn(para.size1,para.size2);
 
 
 %% Solver 
-u =  TGVSolver(noised,para);
+u1 =  TGVSolver(noised,para);
 
-% u = PreDouglasL1(noised,para);
+u2 = PreDouglasL1(noised,para);
 
 % u = PreDouglasL2(noised,para);
 
 % u = SemiNewton(noised,para);
 
+result.MSE_TGV = sum(sum((u1 - f).^2))/para.size1/para.size2;
+result.PSNR_TGV = 10*log10(max(max(u1))^2/result.MSE_TGV);
+
+result.MSE_PDL2 = sum(sum((u2 - f).^2))/para.size1/para.size2;
+result.PSNR_PDL2 = 10*log10(max(max(u2))^2/result.MSE_PDL2);
+
+result.MSE_Origin = sum(sum((noised -f).^2))/para.size1/para.size2;
+result.PSNR_Origin = 10*log10(max(max(noised))^2/result.MSE_Origin);
+
+
+
+
 %%
-subplot(1,3,1);
+subplot(1,4,1);
+set(gcf,'Position',[40,40,1600,200]);
 imshow(f);
-subplot(1,3,2)
+h = title('Origin');
+set(h,'fontsize',20);
+
+subplot(1,4,2)
 imshow(noised);
-subplot(1,3,3)
-imshow(u);
+h = title({'noised';['PSNR =',num2str(result.PSNR_Origin)]});
+set(h,'fontsize',20);
+
+subplot(1,4,3)
+imshow(u1);
+h = title({'TGV';['PSNR =',num2str(result.PSNR_TGV)]});
+set(h,'fontsize',20);
+
+subplot(1,4,4)
+imshow(u2);
+h = title({'PDL2';['PSNR =',num2str(result.PSNR_PDL2)]});
+set(h,'fontsize',20);
+
+saveas(gcf,'./result/result_TGV_peppers.png');
